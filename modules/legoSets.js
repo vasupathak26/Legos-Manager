cbf4dc7e96b80c0bac414fd0e1cad964b1c26a7e
Vasu Shaun Pathak <vpathak2@myseneca.ca>
2023-11-20 02:52:46 -0500

    added bulkload to get data but having error

--- modules/legoSets.js
+++ modules/legoSets.js
@@ -19,6 +19,38 @@ sequelize
   .catch((err) => {
     console.log('Unable to connect to the database:', err);
   });
+const Theme = sequelize.define('Theme',
+ {
+  id: {
+    type:  Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true, 
+  },
+  name: Sequelize.STRING,
+},
+  {
+    createdAt: false,
+    updatedAt: false, 
+  }
+);
+const Set = sequelize.define('Set', 
+{
+  set_num: {
+    type: Sequelize.STRING,
+    primaryKey: true,
+  },
+  name: Sequelize.STRING,
+  year: Sequelize.INTEGER,
+  num_parts: Sequelize.INTEGER,
+  theme_id: Sequelize.INTEGER, // Ensure this matches the data type in Theme model
+  img_url: Sequelize.STRING,
+}, {
+  createdAt: false,
+  updatedAt: false, 
+});
+
+ Set.belongsTo(Theme, {foreignKey: 'theme_id'})
+ 
   let sets = [];  
 function initialize() {
     return new Promise((resolve,reject)=>{
@@ -67,3 +99,29 @@ function initialize() {
     })
   }
   module.exports = { initialize, getAllSets, getSetByNum, getSetsByTheme }
+  sequelize
+  .sync()
+  .then( async () => {
+    try{
+      await Theme.bulkCreate(themeData);
+      await Set.bulkCreate(setData); 
+      console.log("-----");
+      console.log("data inserted successfully");
+    }catch(err){
+      console.log("-----");
+      console.log(err.message);
+
+      // NOTE: If you receive the error:
+
+      // insert or update on table "Sets" violates foreign key constraint "Sets_theme_id_fkey"
+
+      // it is because you have a "set" in your collection that has a "theme_id" that does not exist in the "themeData".   
+
+      // To fix this, use PgAdmin to delete the newly created "Themes" and "Sets" tables, fix the error in your .json files and re-run this code
+    }
+
+    process.exit();
+  })
+  .catch((err) => {
+    console.log('Unable to connect to the database:', err);
+  });
