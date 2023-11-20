7a343d522ad5d2a685ee92e53ed04f7f88d93d90
Vasu Shaun Pathak <vpathak2@myseneca.ca>
2023-11-20 01:32:23 -0500

    added some code to connect with database

--- modules/legoSets.js
+++ modules/legoSets.js
@@ -1,6 +1,25 @@
 const setData = require("../data/setData");
 const themeData = require("../data/themeData");
-let sets = [];  
+require('dotenv').config();
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: 'postgres',
+  port: 5432,
+  dialectOptions: {
+    ssl: { rejectUnauthorized: false },
+  },
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((err) => {
+    console.log('Unable to connect to the database:', err);
+  });
+  let sets = [];  
 function initialize() {
     return new Promise((resolve,reject)=>{
         setData.forEach((setElement) => {
