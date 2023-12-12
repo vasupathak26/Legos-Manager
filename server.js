ba4c23cf5ba10736adf1dd234537176404a55fe4
Vasu Shaun Pathak <vpathak2@myseneca.ca>
2023-12-12 00:42:43 -0500

    updated

--- server.js
+++ server.js
@@ -25,10 +25,10 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   clientSessions({
-    cookieName: 'session', // this is the object name that will be added to 'req'
-    secret: 'o6LjQ5EVNC28ZgK64hDELM18ScpFQr', // this should be a long un-guessable string.
-    duration: 2 * 60 * 1000, // duration of the session in milliseconds (2 minutes)
-    activeDuration: 1000 * 60, // the session will be extended by this many ms each request (1 minute)
+    cookieName: 'session', 
+    secret: 'o6LjQ5EVNC28ZgK64hDELM18ScpFQr', 
+    duration: 2 * 60 * 1000, 
+    activeDuration: 1000 * 60, 
   })
 );
 app.use((req, res, next) => {
